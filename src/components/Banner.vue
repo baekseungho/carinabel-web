6a723dd1fc9267a9d410b20f89cec75733dafc4c
baekseungho <vudrkd1580@naver.com>
2025-05-13 02:03:41 +0900

    25.05.13 스크립트 방지 및 배너 스타일수정

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -17,10 +17,6 @@
                 </div>
             </SwiperSlide>
         </Swiper>
-
-        <!-- <div class="swiper-button-next"></div>
-        <div class="swiper-button-prev"></div> -->
-        <div class="swiper-pagination"></div>
     </div>
 </template>
 
@@ -44,6 +40,8 @@ const filteredBanners = computed(() =>
     height: 600px;
     overflow: hidden;
     position: relative;
+    --swiper-navigation-color: #cc8a94; /* 네비게이션 버튼 색상 */
+    --swiper-pagination-color: #cc8a94; /* 페이지네이션 색상 */
 }
 
 .swiper {
@@ -54,7 +52,7 @@ const filteredBanners = computed(() =>
 .slide-content {
     width: 100%;
     height: 100%;
-    background-size: contain;
+    background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
     display: flex;
@@ -64,39 +62,5 @@ const filteredBanners = computed(() =>
     font-size: 48px;
     font-weight: bold;
     text-shadow: 2px 2px 8px rgba(0, 0, 0, 0.7);
-}
-
-.swiper-button-next,
-.swiper-button-prev {
-    color: #fff;
-    font-size: 24px;
-    z-index: 10;
-    position: absolute;
-    top: 50%;
-    transform: translateY(-50%);
-    background-color: rgba(0, 0, 0, 0.5);
-    border-radius: 50%;
-    width: 40px;
-    height: 40px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    cursor: pointer;
-}
-
-.swiper-button-next {
-    right: 10px;
-}
-
-.swiper-button-prev {
-    left: 10px;
-}
-
-.swiper-pagination {
-    position: absolute;
-    bottom: 20px;
-    left: 50%;
-    transform: translateX(-50%);
-    z-index: 10;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,9 @@
 <template>
     <section class="home">
         <Banner />
-        <div class="container">
+        <div class="">
             <div class="product-grid">
+                상품및 홍보할 공간
                 <ProductCard
                     v-for="product in products"
                     :key="product.id"
@@ -32,7 +33,14 @@ onMounted(() => {
 <style scoped>
 .product-grid {
     display: flex;
-    flex-wrap: wrap;
-    gap: 20px;
+    /* flex-wrap: wrap; */
+    /* gap: 20px; */
+    border: 2px solid #cc8a94;
+    width: 100%;
+    height: 1200px;
+    color: #cc8a94;
+    align-items: center;
+    justify-content: center;
+    font-size: 4rem;
 }
 </style>
