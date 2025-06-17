db5e6a858f3fcd1ad04888906ec73f85737f2432
shbaek <shbaek@dainlab.com>
2025-06-17 17:54:07 +0900

    25.06.17 수정

--- src/adminViews/AdminCreateView.vue
+++ src/adminViews/AdminCreateView.vue
@@ -2,32 +2,33 @@
 <template>
     <div style="max-width: 400px; margin: 100px auto">
         <h2>관리자 계정 생성</h2>
-        <form @submit.prevent="handleCreate">
-            <div>
-                <label>이름</label>
-                <input type="text" v-model="fullName" />
-            </div>
-            <div>
-                <label>회원번호</label>
-                <input type="text" v-model="memberId" />
-            </div>
-            <div>
-                <label>비밀번호</label>
-                <input type="password" v-model="password" />
-            </div>
-            <button type="submit">생성</button>
-        </form>
+
+        <div>
+            <label>이름</label>
+            <input type="text" v-model="fullName" />
+        </div>
+        <div>
+            <label>회원번호</label>
+            <input type="text" v-model="memberId" />
+        </div>
+        <div>
+            <label>비밀번호</label>
+            <input type="password" v-model="password" />
+        </div>
+        <button @click="handleAdmiCreate">생성</button>
     </div>
 </template>
 
 <script setup>
 import { ref } from "vue";
 import axios from "axios";
+import { useRoute } from "vue-router";
+import AdminService from "@/api/AdminService";
 
+const router = useRoute();
 const fullName = ref("");
 const memberId = ref("");
 const password = ref("");
-
 const handleCreate = async () => {
     try {
         const res = await axios.post("http://localhost:5000/api/admin/create", {
@@ -39,5 +40,32 @@ const handleCreate = async () => {
     } catch (err) {
         alert("❌ 관리자 생성 실패: " + err.response?.data?.message);
     }
+};
+
+const handleAdmiCreate = () => {
+    if (!memberId.value.trim()) {
+        alert("관리자 ID를 입력해주세요.");
+        return;
+    }
+    if (!password.value.trim()) {
+        alert("비밀번호를 입력해주세요.");
+        return;
+    }
+
+    const data = {
+        fullName: fullName.value,
+        memberId: memberId.value,
+        password: password.value,
+    };
+
+    AdminService.adminCreate(data)
+        .then((response) => {
+            alert("✅ 관리자 생성 성공: " + response.data.memberId);
+            router.push("/admin/login");
+        })
+        .catch((error) => {
+            console.error(error);
+            alert("❌ 관리자 생성 실패: " + error.response?.data?.message);
+        });
 };
 </script>
--- src/api/AdminService.js
+++ src/api/AdminService.js
@@ -5,139 +5,138 @@ const API_URL = "/qna";
 const ADMIN_API_URL = "/admin";
 
 class AdminService {
-  answerQna(qnaId, data, token) {
-    return axios.put(`${API_URL}/answer/${qnaId}`, data, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-  }
+    answerQna(qnaId, data, token) {
+        return axios.put(`${API_URL}/answer/${qnaId}`, data, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+    }
 
-  getAllQnas(token) {
-    return axios.get(`${API_URL}/admin`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-  }
-  adminLogin(data) {
-    return axios.post(`${ADMIN_API_URL}/login`, data);
-  }
-  getFilteredUsers(params, token) {
-    return axios.get("/admin/users", {
-      headers: { Authorization: `Bearer ${token}` },
-      params,
-    });
-  }
+    getAllQnas(token) {
+        return axios.get(`${API_URL}/admin`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+    }
+    adminLogin(data) {
+        return axios.post(`${ADMIN_API_URL}/login`, data);
+    }
+    adminCreate(data) {
+        return axios.post(`${ADMIN_API_URL}/create`, data);
+    }
+    getFilteredUsers(params, token) {
+        return axios.get("/admin/users", {
+            headers: { Authorization: `Bearer ${token}` },
+            params,
+        });
+    }
 
-  getAllOrders(token, params) {
-    return axios.get("/admin/orders", {
-      headers: { Authorization: `Bearer ${token}` },
-      params,
-    });
-  }
+    getAllOrders(token, params) {
+        return axios.get("/admin/orders", {
+            headers: { Authorization: `Bearer ${token}` },
+            params,
+        });
+    }
 
-  getAllProducts(token) {
-    return axios.get(`${ADMIN_API_URL}/products`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-  }
+    getAllProducts(token) {
+        return axios.get(`${ADMIN_API_URL}/products`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+    }
 
-  // 상품 추가
-  addProduct(productData, token) {
-    return axios.post(`${ADMIN_API_URL}/products/add`, productData, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-  }
+    // 상품 추가
+    addProduct(productData, token) {
+        return axios.post(`${ADMIN_API_URL}/products/add`, productData, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+    }
 
-  // 상품 삭제
-  deleteProduct(productId, token) {
-    return axios.delete(`${ADMIN_API_URL}/products/delete/${productId}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-  }
+    // 상품 삭제
+    deleteProduct(productId, token) {
+        return axios.delete(`${ADMIN_API_URL}/products/delete/${productId}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+    }
 
-  // 상품 수정
-  updateProduct(productId, updatedData, token) {
-    return axios.put(
-      `${ADMIN_API_URL}/products/update/${productId}`,
-      updatedData,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-  }
+    // 상품 수정
+    updateProduct(productId, updatedData, token) {
+        return axios.put(`${ADMIN_API_URL}/products/update/${productId}`, updatedData, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+    }
 
-  // 키트 전체 조회
-  getKits(token) {
-    return axios.get(`${ADMIN_API_URL}/kits`, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-  }
+    // 키트 전체 조회
+    getKits(token) {
+        return axios.get(`${ADMIN_API_URL}/kits`, {
+            headers: { Authorization: `Bearer ${token}` },
+        });
+    }
 
-  // 키트 추가
-  createKit(kitData, token) {
-    return axios.post(`${ADMIN_API_URL}/kits/create`, kitData, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-  }
+    // 키트 추가
+    createKit(kitData, token) {
+        return axios.post(`${ADMIN_API_URL}/kits/create`, kitData, {
+            headers: { Authorization: `Bearer ${token}` },
+        });
+    }
 
-  // 키트 수정
-  updateKit(kitId, updatedData, token) {
-    return axios.put(`${ADMIN_API_URL}/kits/${kitId}`, updatedData, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-  }
-  // 키트 삭제
-  deleteKit(kitId, token) {
-    return axios.delete(`${ADMIN_API_URL}/kits/${kitId}`, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-  }
+    // 키트 수정
+    updateKit(kitId, updatedData, token) {
+        return axios.put(`${ADMIN_API_URL}/kits/${kitId}`, updatedData, {
+            headers: { Authorization: `Bearer ${token}` },
+        });
+    }
+    // 키트 삭제
+    deleteKit(kitId, token) {
+        return axios.delete(`${ADMIN_API_URL}/kits/${kitId}`, {
+            headers: { Authorization: `Bearer ${token}` },
+        });
+    }
 
-  getReferralEarningsList = (token, params) => {
-    return axios.get(`${ADMIN_API_URL}/referral-earnings`, {
-      headers: { Authorization: `Bearer ${token}` },
-      params,
-    });
-  };
-  getReferralDetails = (token, userId) => {
-    return axios.get(`${ADMIN_API_URL}/referral-details/${userId}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-  };
+    getReferralEarningsList = (token, params) => {
+        return axios.get(`${ADMIN_API_URL}/referral-earnings`, {
+            headers: { Authorization: `Bearer ${token}` },
+            params,
+        });
+    };
+    getReferralDetails = (token, userId) => {
+        return axios.get(`${ADMIN_API_URL}/referral-details/${userId}`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
+    };
 
-  // 수당 지급
-  payReferral(data, token) {
-    return axios.post(`${ADMIN_API_URL}/referral-pay`, data, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-  }
+    // 수당 지급
+    payReferral(data, token) {
+        return axios.post(`${ADMIN_API_URL}/referral-pay`, data, {
+            headers: { Authorization: `Bearer ${token}` },
+        });
+    }
 
-  getAdminOrders(params, token) {
-    return axios.get(`${ADMIN_API_URL}/admin-orders`, {
-      headers: { Authorization: `Bearer ${token}` },
-      params,
-    });
-  }
+    getAdminOrders(params, token) {
+        return axios.get(`${ADMIN_API_URL}/admin-orders`, {
+            headers: { Authorization: `Bearer ${token}` },
+            params,
+        });
+    }
 
-  updateOrderStatus(orderId, status, token) {
-    return axios.put(
-      `${ADMIN_API_URL}/update-order-status/${orderId}`,
-      { status },
-      { headers: { Authorization: `Bearer ${token}` } }
-    );
-  }
+    updateOrderStatus(orderId, status, token) {
+        return axios.put(
+            `${ADMIN_API_URL}/update-order-status/${orderId}`,
+            { status },
+            { headers: { Authorization: `Bearer ${token}` } }
+        );
+    }
 }
 
 export default new AdminService();
